603c40c6720017936bba775c5fca7009022b75b3
Lucasfth <luha@itu.dk>
2023-11-30 05:14:44 +0100

    Init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Box from "@mui/material/Box"
+import Button from "@mui/material/Button"
+
+function App() {
+  return (
+    <Box sx={{boarderWidth: 1}}>
+      Hej
+      <Button variant="contained">Hello World</Button>
+    </Box>
+  )
+}
+
+export default App
